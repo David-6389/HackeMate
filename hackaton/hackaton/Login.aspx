b3aaf92502a89750ff7a3d27d9ebea2a0937b7ba
Florencia Alberto <flopy_2792@hotmail.com.ar>
2020-12-10 16:51:20 -0300

    agregado

---
+++ hackaton/hackaton/Login.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="hackaton.contact" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="width: 253px; height: 228px">
+    <form id="form1" runat="server">
+        <div>
+            Nombre de Usuario <br>
+            <asp:TextBox ID="TextBox1" runat="server" Width="229px"></asp:TextBox>
+        </div>
+        <p>
+            Contraseña <br>
+            <asp:TextBox ID="TextBox2" runat="server" Width="230px"></asp:TextBox>
+            <asp:LinkButton ID="LinkButton1" runat="server">Olvide mi contraseña</asp:LinkButton>
+        </p>
+        
+       <div>
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click1" Text="Registro" Width="100px" />
+        <asp:Button ID="Button3" runat="server" Text="Ingresar" Width="100px" OnClick="Button3_Click" />
+          </div>
+       
+        <br>
+        
+    </form>
+
+
+</body>
+</html>
---
+++ hackaton/hackaton/Login.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Data.SqlClient;
+
+
+
+namespace hackaton
+{
+    public partial class contact : System.Web.UI.Page
+    {
+
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-LQIJIH8;Initial Catalog=Hakaton;Persist Security Info=True;User ID=sa;Password=123");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+
+            Response.Redirect("Registro.aspx");
+            
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button2_Click1(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int contador = 0;
+                while (contador <= 3)
+                {
+
+
+                    string uid = TextBox1.Text;
+                    string pass = TextBox2.Text;
+                    con.Open();//aqui
+                               //trae mi usuario si coincide
+                    string qry = "select * from usuario where[nombreUsuario] = '" + uid + "' and[contrasenia] = '" + pass + "'";
+
+                    SqlCommand cmd = new SqlCommand(qry, con);
+                    SqlDataReader sdr = cmd.ExecuteReader();
+                    if (sdr.Read())
+                    {
+                        //saltar a otra pagina
+                        //Response.Redirect("pagina.aspx");
+                    }
+                    else
+                    {
+
+                        //agregar a cantidad de intentos fallidos
+                        contador++;
+                    }
+
+                }
+
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+    }
+    }
+}
